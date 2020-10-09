0140dc1f46cf2e74ba599b6ddf723e2139bbc71a
ReinanHS <reinangabriel1520@gmail.com>
2020-10-09 17:39:52 -0300

    Modelagem do banco de dados

--- database/migrations/2020_10_09_202824_create_vehicles_table.php
+++ database/migrations/2020_10_09_202824_create_vehicles_table.php
@@ -15,6 +15,11 @@ class CreateVehiclesTable extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
+            $table->string('placa', 7);
+            $table->string('modelo');
+            $table->string('marca');
+            $table->enum('status', ['disponível', 'quebrado', 'manutenção']);
+            $table->string('chassi', 17);
             $table->timestamps();
         });
     }
